022fce58d41ae9293929860f884313fc98fd68f0
ken koontz <kenneth.koontz@gmail.com>
2014-08-19 18:20:54 -0700

    initial commit

---
+++ Laravel/app/views/scratchpad.php
@@ -0,0 +1,16 @@
+<html>
+  <head></head>
+  <body>
+    <!-- Using a do...while statement, create a function that counts to 10. solution -->
+    <?php
+    function print_10() {
+    	$i = 0;
+    	do {
+    		echo $i++;
+    	} while ($i < 10);
+    }
+    print_10();
+    ?>
+    
+  </body>
+</html>
